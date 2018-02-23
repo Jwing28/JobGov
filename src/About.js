4361f6e2ed020cc1c29c7addcff995db0ca3f1f4
Jonathan Lee <jonathanlee@Jims-MacBook-Air.local>
2018-02-22 22:33:34 -0800

    setup layout of application

---
+++ src/About.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default (props) => (
+	<div className={props.about}>
+		<h4>Created by: Jonathan Lee</h4>
+		<span>Personal Site</span>
+		<span>Github</span>
+		<span>Linkedin</span>
+	</div>
+);
