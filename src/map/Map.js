03218d64cd481bd57e0d326850da703b8e1bfd6a
Jonathan Lee <jonathan.wing.lee@gmail.com>
2018-03-01 17:17:31 -0800

    added required google maps api key

--- src/map/Map.js
+++ src/map/Map.js
@@ -15,7 +15,7 @@ import {
 const Map = compose(
   withProps({
     googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyDuJb0JhPVYw37ptXpNO0_TLM-GjNdagik&libraries=geometry,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />
